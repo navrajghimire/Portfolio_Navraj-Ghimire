00db2934c87373af0ab8eb404d48ec252807a21c
navrajghimire <147842648+navrajghimire@users.noreply.github.com>
2023-12-07 23:55:44 -0700

    1

--- js/main.js
+++ js/main.js
@@ -1,51 +1,55 @@
-/*
-//
-//
-//
-//
-*/
-console.log('Hello, World!'); // just saying hello
+  
+ // Footer
+ var currentYear = new Date().getFullYear();
+ document.getElementById('current-year').textContent += currentYear;
 
-/* alert ('Hello, World!'); // just saying hello */
 
- // Footer - Display current year
- const currentYear = new Date().getFullYear();
- document.getElementById('current-year').innerText = currentYear;
 
+
+/* alert */
+console.log('I Apologize for the unintentional mistake. Assuring you this will not be repeated in future....!!!'); // just saying hello
 
   // Alert Button
   document.getElementById('btn-alert').addEventListener('click', function () {
-    alert('Hello world!');
+    alert('I Apologize for the unintentional mistake. Assuring you this will not be repeated in future....!!!');
   });
 
-  // Hover Button
-  const hoverButton = document.getElementById('btn-hover');
+  
 
-  hoverButton.addEventListener('mouseover', function () {
-    hoverButton.innerText = '----- Welcome -----';
-  });
 
-  hoverButton.addEventListener('mouseleave', function () {
-    hoverButton.innerText = 'Click here to login';
-  });
+//Use JavaScript to change the button label, so the text of the button changes when the mouse hovers over it and then reverts when it leaves.
+// Hover Button
+
+var hoverButton = document.getElementById('btn-hover').onmouseover = () => {
+    document.getElementById('btn-hover').innerText = '~ . . welcome . . ~';
+};
+
+document.getElementById('btn-hover').onmouseleave = () => {
+    document.getElementById('btn-hover').innerText = 'Click here to login';
+};
+
 
 
    // Incrementing Button
    let count = 1;
 
-   document.getElementById('btn-counter').addEventListener('click', function () {
+     document.getElementById('btn-counter').addEventListener('click', function () {
      count++;
      document.getElementById('txt-counter').innerText = `Number: ${count}`;
- 
-     // Check if the number is even or odd
-     if (count % 2 === 0) {
-       document.getElementById('txt-counter').classList.remove('odd');
-       document.getElementById('txt-counter').classList.add('even');
-     } else {
-       document.getElementById('txt-counter').classList.remove('even');
-       document.getElementById('txt-counter').classList.add('odd');
-     }
-   });
+
+     
+
+// Check whether the number is even or odd.
+if (count % 2 === 0) {
+  document.getElementById('txt-counter').classList.remove('odd');
+  document.getElementById('txt-counter').classList.add('even');
+} else {
+  document.getElementById('txt-counter').classList.remove('even');
+  document.getElementById('txt-counter').classList.add('odd');
+}
+});
+
+
 
    
   // For Loops
@@ -56,7 +60,7 @@ console.log('Hello, World!'); // just saying hello
     listItem.textContent = i % 2 === 0 ? 'even' : 'odd';
     
     // Change color based on even or odd
-    listItem.style.color = i % 2 === 0 ? 'red' : 'green';
+    listItem.style.color = i % 2 === 0 ? 'rgb(115, 0, 255)' : 'rgb(0, 0, 0)';
 
     numbersList.appendChild(listItem);
   }
